c34b3d5c65d427e5143f70edc04f2ada51333531
cihatar <cihatarman2003@gmail.com>
2026-01-24 14:29:50 +0300

    Add docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,24 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv} from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }: { mode: string }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.BACKEND_URL ? process.env.BACKEND_URL : process.env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  })
+}
